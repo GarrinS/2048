828334e7ed5f659445b23976133788820bfcb39c
Garrin Shieh <garrin.shieh@gmail.com>
2019-04-06 15:21:31 -0700

    Updated intro print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    private static Board board = new Board();
-    private static boolean endGame = false;
+	private static Board board = new Board();
+	private static boolean endGame = false;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Board.Direction direction;
-        System.out.println("Welcome to 2048! Rules are simple; use WASD to move the board around in order to combine like tiles together.");
-        System.out.println("Refer to play2048.co or my ReadMe in Visual Studio Code (the blue x icon) for more information.");
-        for (int i = 0; i < 2; i++) {
-            board.generateNewBlock();
-        }
-        while (!endGame) {
-            if (board.noPossibleMove()) {
-                endGame = true;
-            }
-            System.out.print(board.toString());
-            direction = board.convertKeyToDirection(scanner.next());
-            if (direction == Board.Direction.INVALID) {
-                System.out.println("This direction is invalid. Please try again");
-            } else {
-                board.move(direction);
-                System.out.println(board.getScore());
-                board.generateNewBlock();
-            }
-        }
-        System.out.println("Game over!");
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Board.Direction direction;
+		System.out.println("Welcome to 2048! Rules are simple; use WASD to move the board around in order to combine like tiles together.");
+		System.out.println("Refer to play2048.co for more information.");
+		for (int i = 0; i < 2; i++) {
+			board.generateNewBlock();
+		}
+		while (!endGame) {
+			if (board.noPossibleMove()) {
+				endGame = true;
+			}
+			System.out.print(board.toString());
+			direction = board.convertKeyToDirection(scanner.next());
+			if (direction == Board.Direction.INVALID) {
+				System.out.println("This direction is invalid. Please try again");
+			} else {
+				board.move(direction);
+				System.out.println(board.getScore());
+				board.generateNewBlock();
+			}
+		}
+		System.out.println("Game over!");
+		scanner.close();
+	}
 }
