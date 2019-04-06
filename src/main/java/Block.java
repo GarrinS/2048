8b73881c842a8c13d7a60858419c6071ada5eb37
Garrin Shieh <garrin.shieh@gmail.com>
2019-04-06 14:56:55 -0700

    First commit of my 2048 project

---
+++ src/main/java/Block.java
@@ -0,0 +1,22 @@
+import lombok.*;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Block {
+    @Getter
+    @Setter
+    private int score;
+
+    public boolean equilibrium(Block block) {
+        return block.getScore() == this.getScore();
+    }
+
+    public void merge(Block block) {
+        this.setScore(score + block.getScore());
+    }
+
+    public void clear() {
+        this.setScore(0);
+    }
+}
